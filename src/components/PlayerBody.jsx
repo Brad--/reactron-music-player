a56b71e21bfce7a6decc44759f74a20b719d3fcb
Joshua Fuller <joshuaandrewfuller@gmail.com>
2016-07-28 18:32:12 -0600

    Added crude queue implementation
    Decided to start with the queue instead of a playlist, since a playlist will probably need to interact with the underlying queue structure anyways. Also implemented the beginnings of the queue UI. Right now It's just play list items, but I will make it more stylish later

--- src/components/PlayerBody.jsx
+++ src/components/PlayerBody.jsx
@@ -4,24 +4,56 @@ import SplitPane from 'react-split-pane';
 class PlayerBody extends Component {
   constructor() {
     super();
-    this.state = {url: ""};
+    this.state = {url: "", queue: []};
     this.onDrop = this.onDrop.bind(this);
+    this.onEnded = this.onEnded.bind(this);
   };
   onDrop (ev) {
-    this.setState({url: ev.dataTransfer.files[0].path});
+    var path = ev.dataTransfer.files[0].path;
+    if(this.state.url === "") {
+      this.setState({url: path});
+    }
+    else {
+      var newQueue = this.state.queue;
+      newQueue.push(path);
+      this.setState({queue: newQueue});
+    }
     ev.preventDefault();
   };
+  onEnded () {
+    var newQueue = this.state.queue;
+    if (this.state.queue.length === 0) {
+      this.setState({url: ""});
+    }
+    else {
+      var nextSong = newQueue.shift();
+      this.setState({url: nextSong, queue: newQueue});
+    }
+  };
   render() {
+    var queue = this.state.queue.map(function(song) {
+      return (
+        <li>{song}</li>
+      );
+    });
     return (
       <div className="player-body">
         <div className="menu-bar">
-          <div className="reactron-logo">Reactron</div>
+          <div className="reactron-logo">Reactron Music Player</div>
         </div>
         <div className="playlist">
+          <ul>
+            {queue}
+          </ul>
           <div className="spacer"></div>
-          <div className="drop-area" onDrop={this.onDrop}></div>
+          <div className="drop-area"
+               onDrop={this.onDrop}>
+          </div>
         </div>
-        <audio className="audio-player" src={this.state.url} controls autoPlay />
+        <audio className="audio-player"
+               src={this.state.url}
+               onEnded={this.onEnded}
+               controls autoPlay />
       </div>
     );
   }
